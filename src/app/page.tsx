a0bf4332503888a752877004e62ac0bc6a85ce2e
Maksim Shuklun <makshuk@bk.ru>
2023-11-22 07:54:54 +0300

    add useSWRInfinite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,37 @@
 "use client";
-import Post, { PostBody } from "@/components/card";
-import PostTitle from '@/components/title';
+import { myMiddleware } from "@/lib/get-post";
+import useSWRInfinite from "swr/infinite";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const getKey = (pageIndex: any, previousPageData: any) => {
+  console.log({ pageIndex });
+  console.log({ previousPageData });
+  if (previousPageData && !previousPageData.length) return null; // достигнут конец
+  return `https://jsonplaceholder.typicode.com/posts?_page=${pageIndex}&_limit=10`; // ключ SWR
+};
 
 export default function Home() {
-  const id = [12, 43, 52, 61];
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher, {
+    use: [myMiddleware],
+  });
+  if (!data) return "loading";
+
+  let totalPost = 0;
+  for (let i = 0; i < data.length; i++) {
+    totalPost += data[i].length;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <span> TITLE: </span>
-      <PostTitle id={id[0]}></PostTitle>
-      <span> BODY: </span>
-
-      <PostBody id={id[0]}></PostBody>
+      return{" "}
+      <div>
+        <p>{totalPost} users listed</p>
+        {data.map((post, index) => {
+          return post.map((user:any) => <div key={user.id}>{user.title}</div>);
+        })}
+        <button onClick={() => setSize(size + 1)}>Загрузить ещё</button>
+      </div>
     </main>
   );
 }
--- src/lib/get-post.ts
+++ src/lib/get-post.ts
@@ -1,23 +1,21 @@
 "use client";
-import { createKey } from "next/dist/shared/lib/router/router";
 import useSWR from "swr";
 
 const fetcher = (...args: [RequestInfo, RequestInit?]) =>
   fetch(...args).then((res) => res.json());
 
-function myMiddleware(useSWRNext: (arg0: any, arg1: any, arg2: any) => any) {
+export function myMiddleware(
+  useSWRNext: (arg0: any, arg1: any, arg2: any) => any
+) {
   return (key: any, fetcher: any, config: any) => {
     // До выполнения хука...
-    console.log(" До выполнения хука");
-     console.log("SWR запрос:", key);
+  //  console.log("SWR запрос:", config);
     // Обработка следующего ППО, или хука `useSWR`, если это последнее.
     const swr = useSWRNext(key, fetcher, config);
-    console.log("После выполнения хука");
     // После выполнения хука...
     return swr;
   };
 }
-
 
 export default function usePost(id: number) {
   const { data, error, isLoading, isValidating } = useSWR(
